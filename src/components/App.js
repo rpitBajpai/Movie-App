e0f669b152919830c72b48c940b149bf59b85600
Arpit Bajpai <arpitbajpai4321@gmail.com>
2022-11-24 22:33:10 +0530

    Changing the state

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { data } from '../data';
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
+import { addMovies } from "../actions";
 
 class App extends React.Component {
 
@@ -14,16 +15,15 @@ class App extends React.Component {
     // make API call
     // Dispatch Action
 
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    });
+    store.dispatch(addMovies(data));
+
     console.log('STATE', this.props.store.getState());
   }
 
   render () {
-    const movies = this.props.store.getState();
-    console.log('RENDER');
+    // const movies = this.props.store.getState();
+    const { list } = this.props.store.getState(); // {list:[], favourites:[]}
+    console.log('RENDER', this.props.store.getState());
   return (
     <div className="App">
       <Navbar />
@@ -34,7 +34,8 @@ class App extends React.Component {
         </div>
 
         <div className="list">
-          {movies.map((movie, index) => (
+          {/* {movies.map((movie, index) => ( */}
+          {list.map((movie, index) => (
             <MovieCard movie={movie} key={`movies-${index}`} />
           ))}
         </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,20 @@
-export default function movies (state=[], action){
-    if(action.type === 'ADD_MOVIES'){
-        return action.movies;
+import { ADD_MOVIES } from "../actions";
+
+const initialMoviesState = {
+    list: [],
+    favourites: []
+}
+
+export default function movies (state= initialMoviesState, action){
+    if(action.type === ADD_MOVIES){
+        // return action.movies;
+        return {
+            // spread operator
+        ...state,
+        list: action.movies
+        }
+        
     }
     return state;
 }
+
